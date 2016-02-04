96cbc68a921cc2b06690af511ba47902e7897000
Eduardo Santarelli <eduardo.santarell@studio.unibo.it>
2016-02-05 00:52:27 +0100

    narwhals

--- static/public/js/modules/navigator/NavController.js
+++ static/public/js/modules/navigator/NavController.js
@@ -1,11 +1,9 @@
-NavController.$inject = ['$scope', '$state', '$stateParams', '$mdSidenav'];
+NavController.$inject = ['$scope', '$state', '$stateParams', '$mdDialog', '$mdSidenav'];
 /**
  * $mdSidenav e $mdDialog sono i servizi per interagire
  * rispettivamente con la barra laterale e la finestra modale
  */
-export
-default
-function NavController($scope, $state, $stateParams, $mdSidenav) {
+export default function NavController($scope, $state, $stateParams, $mdDialog, $mdSidenav) {
     var model = this;
 
     model.open = () => $mdSidenav('left').toggle(); // Funzione da invocare per aprire il dialog.
@@ -15,6 +13,7 @@ function NavController($scope, $state, $stateParams, $mdSidenav) {
     model.modeIcon = '?'; // Icona della modalità
     model.openToolbox = _openToolbox;
     model.switchMode = _switchMode;
+    model.about = _about;
 
 
     // Sincronizza l'intestazione della sidebar con la modalità attuale
@@ -35,7 +34,10 @@ function NavController($scope, $state, $stateParams, $mdSidenav) {
         // Setta qualche default se non sappiamo che fare;
         let mode = $stateParams.mode || 'reader';
         $mdSidenav('left').close().then(function() {
-            $state.go('mode.docs.document.tools.tab', {mode: mode, toolId: origin});
+            $state.go('mode.docs.document.tools.tab', {
+                mode: mode,
+                toolId: origin
+            });
         });
     }
 
@@ -44,8 +46,24 @@ function NavController($scope, $state, $stateParams, $mdSidenav) {
      */
     function _switchMode() {
         let newMode = (model.activeMode !== 'Annotator') ? 'annotator' : 'reader';
-        $state.go('.', {mode: newMode});
-        _showNavToolBar({mode: newMode}); //Bisogna refreshare la navbar
+        $state.go('.', {
+            mode: newMode
+        });
+        _showNavToolBar({
+            mode: newMode
+        }); //Bisogna refreshare la navbar
+    }
+
+    /**
+     * Apre la finestra 'about'
+     */
+    function _about(ev) {
+        $mdDialog.show({
+                template: '<iframe width="512" height="384" src="https://www.youtube.com/embed/ykwqXuMPsoc" frameborder="0" allowfullscreen></iframe>',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true
+            });
     }
 
     /**
@@ -54,15 +72,15 @@ function NavController($scope, $state, $stateParams, $mdSidenav) {
      */
     function _showNavToolBar(newState, oldState) {
         if (newState.mode && newState.mode.search('annotator') != -1) {
-                model.activeMode = 'Annotator';
-                model.inactiveMode = 'Passa a Reader';
-                model.modeColor = 'teapot-mode-annotator';
-                model.modeIcon = 'mode_edit';
+            model.activeMode = 'Annotator';
+            model.inactiveMode = 'Passa a Reader';
+            model.modeColor = 'teapot-mode-annotator';
+            model.modeIcon = 'mode_edit';
         } else {
-                model.activeMode = 'Reader';
-                model.inactiveMode = 'Passa ad Annotator';
-                model.modeColor = 'teapot-mode-reader';
-                model.modeIcon = 'import_contacts';
+            model.activeMode = 'Reader';
+            model.inactiveMode = 'Passa ad Annotator';
+            model.modeColor = 'teapot-mode-reader';
+            model.modeIcon = 'import_contacts';
         }
     }
 
